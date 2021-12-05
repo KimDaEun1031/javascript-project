9b8cf471f5587ec2811c81a47b91b06f0648d118
김다은 <73865700+KimDaEun1031@users.noreply.github.com>
2021-12-05 18:22:02 +0900

    complete calendar

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -5,7 +5,7 @@ let month_year = document.querySelector('.month-year');
 const arrow_before = document.querySelector('.before');
 const arrow_after = document.querySelector('.after');
 
-const date = new Date('2012-05-14 10:20:30');
+const date = new Date();
 
 let week = ['sun', 'mon', 'tue', 'wen', 'thu', 'fri', 'sat'];
 
@@ -25,48 +25,131 @@ function WhatMonth(date) {
 }
 
 const tbody = document.querySelector('tbody');
-const tr = document.createElement('tr');
-const clone_tr_1 = tr.cloneNode();
-const clone_tr_2 = tr.cloneNode();
-const clone_tr_3 = tr.cloneNode();
-const clone_tr_4 = tr.cloneNode();
-const clone_tr_5 = tr.cloneNode();
-const clone_tr_6 = tr.cloneNode();
 
 let year = date.getFullYear();
 let month = date.getMonth();
-let lastDay = new Date(year, month+1, 0);
+let firstDay = new Date(year, month, 1);
+let today = new Date();
+var pageFirst = firstDay;
+let pageYear;
+let leapYear=[31,29,31,30,31,30,31,31,30,31,30,31];
+let notLeapYear=[31,28,31,30,31,30,31,31,30,31,30,31];
 
-tbody.appendChild(tr);
-tbody.appendChild(clone_tr_1);
-tbody.appendChild(clone_tr_2);
-tbody.appendChild(clone_tr_3);
-tbody.appendChild(clone_tr_4);
-tbody.appendChild(clone_tr_5);
-tbody.appendChild(clone_tr_6);
-
-// for (let idx = 1; idx < 7; idx++) {
-//     let elName = "clone_tr_" + idx;
-//     (new Function(elName))();
-//     console.log(elName);
-//     tbody.appendChild(elName);
-// }
-
-for (let index = 0; index < 7; index++) {
-    tr.innerHTML += "<th class='day'>" + week[index] + "</th>"
+if(firstDay.getFullYear() % 4 === 0){
+    pageYear = leapYear;
+}else{
+    pageYear = notLeapYear;
 }
 
-let num = 1;
+function showCalendar(){
+    let monthCnt = 100;
+    let cnt = 1;
+    for(var i = 0; i < 6; i++){
+        var $tr = document.createElement('tr');
+        $tr.setAttribute('id', monthCnt);   
+        for(var j = 0; j < 7; j++){
+            if((i === 0 && j < firstDay.getDay()) || cnt > pageYear[firstDay.getMonth()]){
+                var $td = document.createElement('td');
+                $tr.appendChild($td);     
+            }else{
+                var $td = document.createElement('td');
+                $td.textContent = cnt;
+                $td.setAttribute('id', cnt);                
+                $tr.appendChild($td);
+                cnt++;
+            }
+        }
+        monthCnt++;
+        tbody.appendChild($tr);
+    }
+}
+    
+showCalendar();
 
-for (let i = 1; i <= 6; i++) {
-    let elName = "clone_tr_" + i;
-    (new Function(elName))();
-
-    for (let j = 1; j <= 6; j++) {
-        elName.innerHTML += "<td>" + num + "</td>";
-        num++
-    } 
-    // tbody.innerHTML += "<td>" + i + "</td>"
+function removeCalendar(){
+    let catchTr = 100;
+    for(var i = 100; i< 106; i++){
+        var $tr = document.getElementById(catchTr);
+        $tr.remove();
+        catchTr++;
+    }
 }
 
-console.log(document.querySelector('table'));
+function prev(){
+    if(pageFirst.getMonth() === 1){
+        pageFirst = new Date(firstDay.getFullYear()-1, 12, 1);
+        firstDay = pageFirst;
+        if(firstDay.getFullYear() % 4 === 0){
+            pageYear = leapYear;
+        }else{
+            pageYear = notLeapYear;
+        }
+    }else{
+        pageFirst = new Date(firstDay.getFullYear(), firstDay.getMonth()-1, 1);
+        firstDay = pageFirst;
+    }
+    today = new Date(date.getFullYear(), date.getMonth()-1, date.getDate());
+    month_year.textContent = WhatMonth(firstDay) + ' ' + firstDay.getFullYear();
+    removeCalendar();
+    showCalendar();
+    showMain();
+    clickedDate1 = document.getElementById(today.getDate());
+    clickedDate1.classList.add('active');  
+    clickStart();
+}
+
+function next(){
+    if(pageFirst.getMonth() === 12){
+        pageFirst = new Date(firstDay.getFullYear()+1, 1, 1);
+        firstDay = pageFirst;
+        if(first.getFullYear() % 4 === 0){
+            pageYear = leapYear;
+        }else{
+            pageYear = notLeapYear;
+        }
+    }else{
+        pageFirst = new Date(firstDay.getFullYear(), firstDay.getMonth()+1, 1);
+        firstDay = pageFirst;
+    }
+    today = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate());
+    month_year.textContent = WhatMonth(firstDay) + ' ' + firstDay.getFullYear();
+    removeCalendar();
+    showCalendar(); 
+    showMain();
+    clickedDate1 = document.getElementById(today.getDate());
+    clickedDate1.classList.add('active');  
+    clickStart();
+}
+
+function showMain(){
+    day.textContent = whatDay(date);
+    nDay.textContent = date.getDate();
+}
+
+var clickedDate1 = document.getElementById(date.getDate());
+clickedDate1.classList.add('active');
+var tdGroup = [];
+
+function clickStart(){
+    for(let i = 1; i <= pageYear[firstDay.getMonth()]; i++){
+        tdGroup[i] = document.getElementById(i);
+        tdGroup[i].addEventListener('click',changeToday);
+    }
+    console.log(tdGroup);
+}
+
+clickStart();
+
+function changeToday(e){
+    for(let i = 1; i <= pageYear[firstDay.getMonth()]; i++){
+        if(tdGroup[i].classList.contains('active')){
+            tdGroup[i].classList.remove('active');
+        }
+    }
+    clickedDate1 = e.currentTarget;
+    clickedDate1.classList.add('active');
+    today = new Date(firstDay.getFullYear(), firstDay.getMonth(), clickedDate1.id);
+    showMain();
+    day.textContent = week[today.getDay()];
+    nDay.textContent = today.getDate();
+}
